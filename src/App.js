e85ebc84a7fb454f78bc7c599a1588eb6569405f
Jason  Taylor <jmt9@TwentyFour.local>
2017-03-16 16:30:31 -0400

    Added image gallery

--- src/App.js
+++ src/App.js
@@ -2,8 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
+
 class App extends Component {
+  handleImageLoad(event) {
+    console.log('Image loaded ', event.target)
+  }
+  
   render() {
+    const images = [
+      {
+        original: 'http://lorempixel.com/1000/600/nature/1/',
+        thumbnail: 'http://lorempixel.com/250/150/nature/1/',
+      },
+      {
+        original: 'http://lorempixel.com/1000/600/nature/2/',
+        thumbnail: 'http://lorempixel.com/250/150/nature/2/'
+      },
+      {
+        original: 'http://lorempixel.com/1000/600/nature/3/',
+        thumbnail: 'http://lorempixel.com/250/150/nature/3/'
+      }
+    ]
+
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +35,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1> Slideshow </h1>
+        <ImageGallery
+          items={images}
+          slideInterval={2000}
+          onImageLoad={this.handleImageLoad} />
       </div>
     );
   }
